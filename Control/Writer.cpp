ef7d2329ee091732e280a82811b20c49dff73a5e
Mateusz Wykurz <wykurz@gmail.com>
2016-05-21 22:13:23 -0400

    Move StatsScope to inner scope so RAII kicks in and we can see the results of going out-of-scope

--- Control/Writer.cpp
+++ Control/Writer.cpp
@@ -14,16 +14,12 @@ namespace Control
 
     void Writer::run(ThreadArray& threadArray_)
     {
-        DLOG() << "TEST";
         for (auto& holder : threadArray_) {
-            DLOG() << "here1";
             auto lk = holder.lock();
             if (!holder.thread) continue;
-            DLOG() << "here2";
             auto& thread = *(holder.thread);
             auto recordNode = thread.template getRecordManager<Record::Record>().extractDirtyRecords();
             while (recordNode) {
-                DLOG() << "here3";
                 _out->get() << recordNode->value;
                 recordNode = recordNode->next;
             }
