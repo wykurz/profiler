14da6b897cd94e37b6ec0a62c2d8710b6dc84d52
Mateusz Wykurz <wykurz@gmail.com>
2016-11-06 00:34:04 -0400

    Add more FreeMap tests

---
+++ tests/stress/Algorithms/BitMask.cpp
@@ -0,0 +1,20 @@
+#include <Algorithms/BitMask.h>
+#include <Exception/Exception.h>
+#include <boost/test/unit_test.hpp>
+
+namespace Profiler { namespace Algorithms { namespace Test
+{
+
+    BOOST_AUTO_TEST_SUITE(BitMaskStressTests)
+
+    BOOST_AUTO_TEST_CASE(Patterns)
+    {
+        // TODO: ...
+        BOOST_CHECK(true);
+    }
+
+    BOOST_AUTO_TEST_SUITE_END()
+
+}
+}
+}
--- tests/unit/Algorithms/BitMask.cpp
+++ tests/unit/Algorithms/BitMask.cpp
@@ -2,11 +2,10 @@
 #include <Exception/Exception.h>
 #include <boost/test/unit_test.hpp>
 
-// TODO: Wrap everything in Profiler namespace
 namespace Profiler { namespace Algorithms { namespace Test
 {
 
-    BOOST_AUTO_TEST_SUITE(BitMaskTests)
+    BOOST_AUTO_TEST_SUITE(BitMaskUnitTests)
 
     BOOST_AUTO_TEST_CASE(OneByte)
     {
@@ -23,7 +22,7 @@ namespace Profiler { namespace Algorithms { namespace Test
         BOOST_CHECK_EQUAL(7, bits.lastFree());
     }
 
-    BOOST_AUTO_TEST_CASE(TwoBytes1)
+    BOOST_AUTO_TEST_CASE(TwoBytes)
     {
         FreeMap bits(9);
         BOOST_CHECK_EQUAL(0, bits.firstFree());
@@ -36,6 +35,27 @@ namespace Profiler { namespace Algorithms { namespace Test
         bits.set(1, false);
         BOOST_CHECK_EQUAL(2, bits.firstFree());
         BOOST_CHECK_EQUAL(8, bits.lastFree());
+    }
+
+    BOOST_AUTO_TEST_CASE(EightBytes)
+    {
+        FreeMap bits(8 * 8 + 1);
+        BOOST_CHECK_EQUAL(0, bits.firstFree());
+        BOOST_CHECK_EQUAL(8 * 8, bits.lastFree());
+        bits.set(0, false);
+        BOOST_CHECK_EQUAL(1, bits.firstFree());
+        BOOST_CHECK_EQUAL(8 * 8, bits.lastFree());
+        BOOST_CHECK_EQUAL(false, bits.isFree(0));
+        bits.set(8 * 8, false);
+        BOOST_CHECK_EQUAL(1, bits.firstFree());
+        BOOST_CHECK_EQUAL(8 * 8 - 1, bits.lastFree());
+        // Reverse
+        bits.set(8 * 8, true);
+        BOOST_CHECK_EQUAL(1, bits.firstFree());
+        BOOST_CHECK_EQUAL(8 * 8, bits.lastFree());
+        bits.set(4 * 8, true);
+        BOOST_CHECK_EQUAL(1, bits.firstFree());
+        BOOST_CHECK_EQUAL(8 * 8, bits.lastFree());
     }
 
     BOOST_AUTO_TEST_SUITE_END()
