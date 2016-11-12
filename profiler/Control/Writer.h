2f4fe48d5fb29d3ee134f69295e500b86dcf70f9
Mateusz Wykurz <wykurz@gmail.com>
2016-11-12 17:59:29 -0500

    Use default destructor

--- profiler/Control/Writer.h
+++ profiler/Control/Writer.h
@@ -11,8 +11,7 @@ namespace Profiler { namespace Control
 
     struct Output
     {
-        virtual ~Output()
-        { }
+        virtual ~Output() = default;
         using Ptr = std::unique_ptr<Output>;
         virtual std::ostream& get() = 0;
     };
