24df9bd7d50f86d6ac0e7f8cf50f6ffa184f0798
Mateusz Wykurz <wykurz@gmail.com>
2016-11-12 21:30:44 -0500

    Add a script to build benchmark lib

---
+++ install_benchmark.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -ex
+git clone https://github.com/google/benchmark.git
+cd benchmark
+cmake -DCMAKE_BUILD_TYPE=Release -DBENCHMARK_USE_LIBCXX=ON -DCMAKE_CXX_FLAGS="-I/usr/include/c++/v1" -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang
+make
+sudo make install
