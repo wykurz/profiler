62d734b9715282b02d8942c3bc3d43e8d2fab2fe
Mateusz Wykurz <wykurz@gmail.com>
2016-11-12 23:57:54 -0500

    Move install_benchmark to before_script; fix install_benchmark to produce out-of-source build

--- install_benchmark.sh
+++ install_benchmark.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 set -ex
-if [ -d benchmark ]; then
-    cd benchmark
-    ls
+if [ -d benchmark/build ]; then
+    cd benchmark/build
     sudo make install
 else
     git clone https://github.com/google/benchmark.git
     cd benchmark
-    cmake -DCMAKE_BUILD_TYPE=Release -DBENCHMARK_USE_LIBCXX=ON -DCMAKE_CXX_FLAGS="-I/usr/include/c++/v1" -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang
+    mkdir build
+    cd build
+    cmake -DCMAKE_BUILD_TYPE=Release -DBENCHMARK_USE_LIBCXX=ON -DCMAKE_CXX_FLAGS="-I/usr/include/c++/v1" -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang ..
     make
     sudo make install
 fi
