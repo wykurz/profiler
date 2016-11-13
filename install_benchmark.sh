de9ad40125e394c427418267c8ee0cfc02932cd8
Mateusz Wykurz <wykurz@gmail.com>
2016-11-12 23:00:58 -0500

    Cache the benchmark lib directory

--- install_benchmark.sh
+++ install_benchmark.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 set -ex
-git clone https://github.com/google/benchmark.git
-cd benchmark
-cmake -DCMAKE_BUILD_TYPE=Release -DBENCHMARK_USE_LIBCXX=ON -DCMAKE_CXX_FLAGS="-I/usr/include/c++/v1" -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang
-make
-sudo make install
+if [ -d benchmark ]; then
+    cd benchmark
+    sudo make install
+else
+    git clone https://github.com/google/benchmark.git
+    cd benchmark
+    cmake -DCMAKE_BUILD_TYPE=Release -DBENCHMARK_USE_LIBCXX=ON -DCMAKE_CXX_FLAGS="-I/usr/include/c++/v1" -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang
+    make
+fi
