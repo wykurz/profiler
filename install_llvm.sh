3caad4768090ba47f2f51d897d06aa8c03cf3a0c
Mateusz Wykurz <wykurz@gmail.com>
2016-11-13 23:12:58 -0500

    Fix pathis in llvm build file

--- install_llvm.sh
+++ install_llvm.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 set -ex
-if [ -d benchmark/build ]; then
-  cd llvm
+if [ -d llvm/build ]; then
+  cd llvm/build
   sudo make install-cxxabi install-cxx
 else
-  git clone --depth=1 https://github.com/llvm-mirror/llvm.git llvm-source
-  git clone --depth=1 https://github.com/llvm-mirror/libcxx.git llvm-source/projects/libcxx
-  git clone --depth=1 https://github.com/llvm-mirror/libcxxabi.git llvm-source/projects/libcxxabi
-  mkdir llvm
-  cd llvm
+  git clone --depth=1 https://github.com/llvm-mirror/llvm.git llvm
+  git clone --depth=1 https://github.com/llvm-mirror/libcxx.git llvm/projects/libcxx
+  git clone --depth=1 https://github.com/llvm-mirror/libcxxabi.git llvm/projects/libcxxabi
+  mkdir build
+  cd build
   cmake -DCMAKE_C_COMPILER=clang \
         -DCMAKE_CXX_COMPILER=clang++ \
         -DCMAKE_INSTALL_PREFIX=/usr \
         -DCMAKE_BUILD_TYPE=Release \
-        ../llvm-source
+        ..
   make cxx -j2
   sudo make install-cxxabi install-cxx
 fi
