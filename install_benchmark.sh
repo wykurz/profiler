7a63199f519c5f2e2ca30d0c47f1ca9e383ee9f6
Mateusz Wykurz <wykurz@gmail.com>
2016-11-14 00:19:16 -0500

    Use ld-gold for benchmark builds w/ g++

--- install_benchmark.sh
+++ install_benchmark.sh
@@ -13,6 +13,7 @@ else
           -DCMAKE_CXX_FLAGS="-I/usr/include/c++/v1" \
           -DCMAKE_CXX_COMPILER=${CXX} \
           -DCMAKE_C_COMPILER=${CC} \
+          -DCMAKE_CXX_FLAGS="${BENCHMARK_FLAGS}" \
           ..
     make
     sudo make install
