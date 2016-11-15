6f7ac58da8d8559177f6c98a0536f77781af19cf
Mateusz Wykurz <wykurz@gmail.com>
2016-11-14 23:06:22 -0500

    Get rid of caching in Travis CI, it doesn't appar to work and if it did, I would need to support build variants

--- install_benchmark.sh
+++ install_benchmark.sh
@@ -1,19 +1,14 @@
 #!/bin/sh
 set -ex
-if [ -d benchmark/build ]; then
-    cd benchmark/build
-    sudo make install
-else
-    git clone https://github.com/google/benchmark.git
-    cd benchmark
-    mkdir build
-    cd build
-    cmake -DCMAKE_BUILD_TYPE=Release \
-          -DCMAKE_CXX_FLAGS="-I/usr/include/c++/v1" \
-          -DCMAKE_CXX_COMPILER=${CXX} \
-          -DCMAKE_C_COMPILER=${CC} \
-          -DCMAKE_CXX_FLAGS="${BENCHMARK_FLAGS}" \
-          ..
-    make
-    sudo make install
-fi
+git clone https://github.com/google/benchmark.git
+cd benchmark
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_CXX_FLAGS="-I/usr/include/c++/v1" \
+      -DCMAKE_CXX_COMPILER=${CXX} \
+      -DCMAKE_C_COMPILER=${CC} \
+      -DCMAKE_CXX_FLAGS="${BENCHMARK_FLAGS}" \
+      ..
+make
+sudo make install
--- install_llvm.sh
+++ install_llvm.sh
@@ -1,19 +1,14 @@
 #!/bin/sh
 set -ex
-if [ -d llvm/build ]; then
-  cd llvm/build
-  sudo make install-cxxabi install-cxx
-else
-  git clone --depth=1 https://github.com/llvm-mirror/llvm.git llvm
-  git clone --depth=1 https://github.com/llvm-mirror/libcxx.git llvm/projects/libcxx
-  git clone --depth=1 https://github.com/llvm-mirror/libcxxabi.git llvm/projects/libcxxabi
-  mkdir llvm/build
-  cd llvm/build
-  cmake -DCMAKE_C_COMPILER=${CC} \
-        -DCMAKE_CXX_COMPILER=${CXX} \
-        -DCMAKE_INSTALL_PREFIX=/usr \
-        -DCMAKE_BUILD_TYPE=Release \
-        ..
-  make cxx -j2
-  sudo make install-cxxabi install-cxx
-fi
+git clone --depth=1 https://github.com/llvm-mirror/llvm.git llvm
+git clone --depth=1 https://github.com/llvm-mirror/libcxx.git llvm/projects/libcxx
+git clone --depth=1 https://github.com/llvm-mirror/libcxxabi.git llvm/projects/libcxxabi
+mkdir llvm/build
+cd llvm/build
+cmake -DCMAKE_C_COMPILER=${CC} \
+      -DCMAKE_CXX_COMPILER=${CXX} \
+      -DCMAKE_INSTALL_PREFIX=/usr \
+      -DCMAKE_BUILD_TYPE=Release \
+      ..
+make cxx -j2
+sudo make install-cxxabi install-cxx
