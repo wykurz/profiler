8276c9ce51c28d595248a06227b62f658a24d9f9
Mateusz Wykurz <wykurz@gmail.com>
2016-11-16 23:50:38 -0500

    Move coverage logic to scripts; annotate g++, not clang

--- upload_coverage.sh
+++ upload_coverage.sh
@@ -1,4 +1,8 @@
-lcov --directory . --capture --output-file coverage.info
-lcov --remove coverage.info 'tests/*' --output-file coverage.info
-lcov --list coverage.info
-coveralls-lcov coverage.info
+#!/bin/sh
+set -ex
+if [ -n "$COVERAGE" ]; then
+    lcov --directory . --capture --output-file coverage.info
+    lcov --remove coverage.info 'tests/*' --output-file coverage.info
+    lcov --list coverage.info
+    coveralls-lcov coverage.info
+fi
