ac0d26dd2c3296093e56ee2be15f05355a3dd838
Mateusz Wykurz <wykurz@gmail.com>
2016-12-06 23:55:26 -0500

    Fixes to travis setup

--- upload_coverage.sh
+++ upload_coverage.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -ex
-if [ -n "$COVERAGE" ]; then
+if [ -n "${COVERAGE}" ]; then
     lcov --gcov-tool $GCOV --directory . --capture --output-file coverage.info
     lcov --gcov-tool $GCOV --remove coverage.info 'tests/*' '/usr/*' --output-file coverage.info
     lcov --gcov-tool $GCOV --list coverage.info
