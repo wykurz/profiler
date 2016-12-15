a548b71d3281c235ff11b4e5aad2dbf10cc2ae94
Mateusz Wykurz <wykurz@gmail.com>
2016-12-14 23:37:45 -0500

    First steps in setting up serialization of records

--- profiler/Record/Record.h
+++ profiler/Record/Record.h
@@ -4,14 +4,15 @@
 #include <Queue/Queue.h>
 #include <array>
 #include <chrono>
+#include <cstring>
 #include <ostream>
+#include <istream>
 
 namespace Profiler { namespace Record
 {
 
     using TimeDelta = std::chrono::duration<double>;
 
-    // TODO: For different record types will need to provide some kind of registration to distinguish after serialized
     struct Record
     {
         Record() = default;
@@ -25,12 +26,27 @@ namespace Profiler { namespace Record
 
     inline std::ostream& operator<<(std::ostream& out_, const Record& record_)
     {
-        out_ << record_.name << record_.delta.count();
+        out_ << strlen(record_.name) << record_.name << record_.delta.count();
         return out_;
     }
 
-    // TODO: Add a type list type that has all records available
-    //       We'll need a for_each functionality...
+    struct OutRecord
+    {
+        OutRecord(const std::string& name_, const TimeDelta& delta_)
+          : name(name_),
+            delta(delta_)
+        { }
+        // TODO: register here?
+        // Use Record's type index?
+        std::string name;
+        TimeDelta delta;
+    };
+
+    // TODO
+    inline std::istream& operator>>(std::istream& in_, OutRecord& record_)
+    {
+	return in_;
+    }
 
 }
 }
