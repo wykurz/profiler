6b6576b68d9f335eafd75a56384b2a1eaf63f90c
Mateusz Wykurz <wykurz@gmail.com>
2017-01-15 22:38:00 -0500

    More more fixes

--- install_boost.sh
+++ install_boost.sh
@@ -4,14 +4,16 @@ wget https://sourceforge.net/projects/boost/files/boost/1.62.0/boost_1_62_0.tar.
 mv download boost_1_62_0.tar.bz2
 tar xf boost_1_62_0.tar.bz2
 cd boost_1_62_0/
-./bootstrap.sh --with-libraries=test,filesystem
 if [[ "${CXX_COMPILER}" == *"clang"* ]]; then
-    TOOLSET='clang'
-    alias clang=${CXX_COMPILER}
+    TOOLSET="clang"
+    alias clang=${C_COMPILER}
+    alias clang++=${CXX_COMPILER}
 else
     TOOLSET="gcc"
-    alias gcc=${CXX_COMPILER}
+    alias gcc=${C_COMPILER}
+    alias g++=${CXX_COMPILER}
 fi
+./bootstrap.sh --with-libraries=test,filesystem --with-toolset=${TOOLSET}
 if [ "${LIBCXX}" == "1" ]; then
     FLAGS="-stdlib=libc++"
     sudo ./b2 install toolset=${TOOLSET} cxxflags=${FLAGS} linkflags=${FLAGS} -j 2
