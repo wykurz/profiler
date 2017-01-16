14908dc0da948c76fe6faf4a94ca860c12b1c211
Mateusz Wykurz <wykurz@gmail.com>
2017-01-15 21:50:26 -0500

    Attempt building boost myself

---
+++ install_boost.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -ex
+wget https://sourceforge.net/projects/boost/files/boost/1.62.0/boost_1_62_0.tar.bz2/download
+mv download boost_1_62_0.tar.bz2
+tar xf boost_1_62_0.tar.bz2
+cd boost_1_62_0/
+./bootstrap.sh --with-libraries=test,filesystem
+./b2 install -j 2
