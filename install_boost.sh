a7f64119d5af534e92718d5d0d0e7efc4b540f66
Mateusz Wykurz <wykurz@gmail.com>
2017-01-15 22:56:57 -0500

    I hate Travis

--- install_boost.sh
+++ install_boost.sh
@@ -6,17 +6,13 @@ tar xf boost_1_62_0.tar.bz2
 cd boost_1_62_0/
 if [[ "${CXX_COMPILER}" == *"clang"* ]]; then
     TOOLSET="clang"
-    alias clang=${C_COMPILER}
-    alias clang++=${CXX_COMPILER}
 else
     TOOLSET="gcc"
-    alias gcc=${C_COMPILER}
-    alias g++=${CXX_COMPILER}
 fi
 ./bootstrap.sh --with-libraries=test,filesystem --with-toolset=${TOOLSET}
 if [ "${LIBCXX}" == "1" ]; then
     FLAGS="-stdlib=libc++"
-    sudo ./b2 install toolset=${TOOLSET} cxxflags=${FLAGS} linkflags=${FLAGS} -j 2
+    sudo PATH=${PATH} ./b2 install toolset=${TOOLSET} cxxflags=${FLAGS} linkflags=${FLAGS} -j 2
 else
-    sudo ./b2 install toolset=${TOOLSET} -j 2
+    sudo PATH=${PATH} ./b2 install toolset=${TOOLSET} -j 2
 fi
