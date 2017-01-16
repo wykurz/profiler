71d54f04a0f311a0790e8bf56f5406a44a9314b8
Mateusz Wykurz <wykurz@gmail.com>
2017-01-15 22:07:53 -0500

    Fixes to install boost script; move it to front temporarily

--- install_boost.sh
+++ install_boost.sh
@@ -5,4 +5,12 @@ mv download boost_1_62_0.tar.bz2
 tar xf boost_1_62_0.tar.bz2
 cd boost_1_62_0/
 ./bootstrap.sh --with-libraries=test,filesystem
-./b2 install -j 2
+TOOLSET="gcc"
+if [[ "${CXX_COMPILER}" == *"clang"* ]]; then
+    TOOLSET='clang'
+fi
+FLAGS=""
+if [ "${LIBCXX}" == "1" ]; then
+    FLAGS="-stdlib=libc++"
+fi
+sudo ./b2 install toolset=${TOOLSET} cxxflags=${FLAGS} linkflags=${FLAGS} -j 2
