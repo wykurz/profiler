5e03f8178e1944568d66322333d3d322844640a3
Mateusz Wykurz <wykurz@gmail.com>
2017-01-28 11:55:16 -0500

    Update stress tests with new changes

--- tests/stress/Arena/Arena.cpp
+++ tests/stress/Arena/Arena.cpp
@@ -11,7 +11,7 @@ namespace Profiler { namespace Control { namespace Test
     BOOST_AUTO_TEST_CASE(RandomWalk)
     {
         const std::size_t nblocks = 10000;
-        Arena arena(sizeof(Arena::BlockHolder) * nblocks);
+        Arena arena(sizeof(Arena::Block) * nblocks);
         std::unordered_set<std::uintptr_t> dirty;
         auto verifyAcquire = [&dirty](void* ptr_) {
             auto iptr = reinterpret_cast<std::uintptr_t>(ptr_);
@@ -27,13 +27,14 @@ namespace Profiler { namespace Control { namespace Test
             {
                 std::vector<std::uintptr_t> dvec(dirty.begin(), dirty.end());
                 std::sort(dvec.begin(), dvec.end());
-                for (int i = 0; i < nblocks - 1; ++i) BOOST_CHECK(sizeof(Arena::BlockHolder) <= dvec[i + 1] - dvec[i]);
+                for (int i = 0; i < nblocks - 1; ++i) BOOST_CHECK(sizeof(Arena::Block) <= dvec[i + 1] - dvec[i]);
             }
             auto randomErase = [&arena, &dirty]() {
                 for (auto it = dirty.begin(); it != dirty.end();) {
                     bool remove = std::rand() & 1;
                     if (remove) {
-                        arena.release(reinterpret_cast<Arena::Block<int>*>(*it));
+                        // arena.release(reinterpret_cast<Arena::Block<int>*>(*it));
+                        arena.release(reinterpret_cast<int*>(*it));
                         it = dirty.erase(it);
                     }
                     else ++it;
@@ -44,7 +45,7 @@ namespace Profiler { namespace Control { namespace Test
                 for (int i = 0; i < 10; ++i) BOOST_REQUIRE(verifyAcquire(arena.acquire<int>()));
                 auto it = dirty.begin();
                 for (int i = 0; i < 10; ++i) {
-                    arena.release(reinterpret_cast<Arena::Block<int>*>(*it));
+                    arena.release(reinterpret_cast<int*>(*it));
                     it = dirty.erase(it);
                 }
             }
