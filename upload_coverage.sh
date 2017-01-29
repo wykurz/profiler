90c02ec63051b930288b1695ee0dc510e99b230f
Mateusz Wykurz <wykurz@gmail.com>
2017-01-28 22:26:50 -0500

    Don't run coverage on boost libraries

--- upload_coverage.sh
+++ upload_coverage.sh
@@ -2,7 +2,7 @@
 set -ex
 if [ -n "${COVERAGE}" ]; then
     lcov --gcov-tool $GCOV --directory . --capture --output-file coverage.info
-    lcov --gcov-tool $GCOV --remove coverage.info 'tests/*' '/usr/*' --output-file coverage.info
+    lcov --gcov-tool $GCOV --remove coverage.info 'tests/*' '/usr/*' '*/boost/*' --output-file coverage.info
     lcov --gcov-tool $GCOV --list coverage.info
     coveralls-lcov coverage.info
 fi
