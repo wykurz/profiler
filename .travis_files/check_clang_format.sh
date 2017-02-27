9a53806d0b982c83c1e35f9a302551ccfd3d28ec
Mateusz Wykurz <wykurz@gmail.com>
2017-02-26 22:13:35 -0500

    One more attempt to fix clang-format

--- .travis_files/check_clang_format.sh
+++ .travis_files/check_clang_format.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-FILES=$(find Profiler tests examples -name *.cpp) $(find Profiler tests examples -name *.h)
+FILES="$(find Profiler tests examples -name *.cpp) $(find Profiler tests examples -name *.h)"
 clang-format-3.8 -style=llvm ${FILES} -output-replacements-xml|grep -c "<replacement " >/dev/null
 if [ $? -ne 1 ]; then
     echo "Commit did not match clang-format"
