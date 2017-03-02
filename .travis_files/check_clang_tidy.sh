52db01fbee018f1d5dc6f446e5a0434cd290f1ca
Mateusz Wykurz <wykurz@gmail.com>
2017-03-01 23:31:19 -0500

    Enable clang-tidy check in travis

--- .travis_files/check_clang_tidy.sh
+++ .travis_files/check_clang_tidy.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-H_FILES=$(find Profiler tests examples -name *.h)
-CPP_FILES=$(find Profiler tests examples -name *.cpp)
-clang-tidy -p=build/debug -config='' ${H_FILES} ${CPP_FILES}
+clang-tidy -p=build/debug -config='' $(find Profiler tests examples -name *.cpp)
