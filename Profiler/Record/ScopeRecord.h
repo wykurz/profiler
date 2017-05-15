b9565438303524821ff87786bca0e7ee0f44b281
Mateusz Wykurz <wykurz@gmail.com>
2017-05-14 22:54:38 -0400

    Apply clang-tidy

--- Profiler/Record/ScopeRecord.h
+++ Profiler/Record/ScopeRecord.h
@@ -23,14 +23,14 @@ template <typename Clock_> struct ScopeStorage {
                std::size_t depth_, std::size_t seqNum_)
       : _name(name_), _t0(std::move(t0_)), _t1(std::move(t1_)), _depth(depth_),
         _seqNum(seqNum_) {}
-  static void encodePreamble(std::ostream &out_) {}
+  static void encodePreamble(std::ostream & /*out_*/) {}
   void encode(std::ostream &out_) {
     Serialize::encodeString(out_, _name);
     out_ << _t0 << _t1;
     Serialize::encode(out_, _depth);
     Serialize::encode(out_, _seqNum);
   }
-  static void decodePreamble(std::istream &in_, std::ostream &out_) {}
+  static void decodePreamble(std::istream & /*in_*/, std::ostream & /*out_*/) {}
   static void decode(std::istream &in_, std::ostream &out_) {
     auto name = Serialize::decodeString(in_);
     Duration t0;
