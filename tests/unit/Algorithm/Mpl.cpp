21311f737cba8cf382ca5c776a0b56c76d304afe
Mateusz Wykurz <wykurz@gmail.com>
2017-06-11 12:00:17 -0400

    Add basic apply for tuple arguments

---
+++ tests/unit/Algorithm/Mpl.cpp
@@ -0,0 +1,24 @@
+#include <Profiler/Algorithm/Mpl.h>
+#include <boost/test/unit_test.hpp>
+#include <sstream>
+#include <tuple>
+
+namespace Profiler {
+namespace Mpl {
+namespace Test {
+
+BOOST_AUTO_TEST_SUITE(MplUnitTests)
+
+BOOST_AUTO_TEST_CASE(ApplyTuple) {
+  std::stringstream ss;
+  apply([&ss](const auto& arg_) {
+      ss << arg_;
+    },
+    std::make_tuple(1, 0.5, "abc"));
+  BOOST_CHECK_EQUAL(ss.str(), "10.5abc");
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+} // namespace Test
+} // namespace Algorithm
+} // namespace Profiler
