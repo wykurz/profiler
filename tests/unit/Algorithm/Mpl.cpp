e19efeba7b13d60c380c589b72519ca281010802
Mateusz Wykurz <wykurz@gmail.com>
2017-06-11 23:32:31 -0400

    Fix a broken test

--- tests/unit/Algorithm/Mpl.cpp
+++ tests/unit/Algorithm/Mpl.cpp
@@ -13,7 +13,7 @@ BOOST_AUTO_TEST_CASE(ApplyTuple) {
   std::stringstream ss;
   auto tup = std::make_tuple(1, 0.5, "abc");
   apply([&ss](const auto &arg_) { ss << arg_; }, tup);
-  BOOST_CHECK_EQUAL(ss.str(), "10.5abc");
+  BOOST_CHECK_EQUAL(ss.str(), "abc0.51");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
